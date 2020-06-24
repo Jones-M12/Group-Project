fd07bd349b6747fe31052cff138373ac1fb0b284
Jones-M <61522666+Jones-M12@users.noreply.github.com>
2020-06-24 18:48:19 -0400

    made changes to edit button and button colors

--- public/js/script.js
+++ public/js/script.js
@@ -15,3 +15,15 @@ document.addEventListener('DOMContentLoaded', function() {
   $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+
+
+//Edit button drop down
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $('.dropdown-trigger').dropdown();
